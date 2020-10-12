955283794c8f1f35cdbdc02c300ab47b64c80487
kouzhonglong <kouzhonglong@comisys.net>
2020-10-12 16:10:46 +0800

    3

--- Aspects.m
+++ Aspects.m
@@ -11,7 +11,7 @@
 #import <objc/message.h>
 
 #define AspectLog(...)
-//#define AspectLog(...) d123o { NSLog(__VA_ARGS__); }while(0)
+//#define AspectLog(...) d1233231o { NSLog(__VA_ARGS__); }while(0)
 #define AspectLogError(...) do { NSLog(__VA_ARGS__); }while(0)
 我单位无
 123
